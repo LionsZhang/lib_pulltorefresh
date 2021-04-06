b8dd3cae2ed13650a9c839329d0486f8ffb750ee
lion <zqhusixia@163.com>
2021-04-06 15:31:05 +0800

    add pulltofresh lib

---
+++ app/src/main/java/com/wowly/common/lib/pulltorefresh/PulltoFreshActivity.kt
@@ -0,0 +1,12 @@
+package com.wowly.common.lib.pulltorefresh
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.wowly.common.lib.pulltorefresh.R.*
+
+class PulltoFreshActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(layout.activity_main)
+    }
+}
